a64aff67b086ecb8a9be40a362a550138486f834
Thorbjørn Hermansen <thhermansen@gmail.com>
2017-09-03 12:58:55 +0200

    duration as hms may hide hours.
    Requirement from min side.

--- tests/integration/helpers/duration-as-hms-test.js
+++ tests/integration/helpers/duration-as-hms-test.js
@@ -10,6 +10,6 @@ test('it renders correctly', function(assert) {
   this.render(hbs`{{duration-as-hms 60}}`);
   assert.equal(this.$().text().trim(), '00:01:00');
 
-  this.render(hbs`{{duration-as-hms 121}}`);
-  assert.equal(this.$().text().trim(), '00:02:01');
+  this.render(hbs`{{duration-as-hms 121 showHours=false}}`);
+  assert.equal(this.$().text().trim(), '02:01');
 });
