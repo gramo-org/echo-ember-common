76057909f041812d46e2e4b8ed4a549d8c2cbc0e
kenglxn <ken@glxn.net>
2018-01-04 20:40:56 +0100

    fix test failure in phantom due to default locale

--- tests/integration/components/molecules/amount-change-test.js
+++ tests/integration/components/molecules/amount-change-test.js
@@ -1,16 +1,18 @@
 import { moduleForComponent, test } from 'ember-qunit'
 import hbs from 'htmlbars-inline-precompile'
+import { toLocaleString } from 'echo-ember-common/helpers/to-locale-string'
 
+/* eslint-disable no-magic-numbers */
 moduleForComponent('molecules/amount-change', 'Integration | Component | molecules/amount change', {
   integration: true
 })
 
 test('it renders', function(assert) {
   this.render(hbs`{{molecules/amount-change new=5 old=10}}`)
-  assert.equal(this.$('p.text--minus').text().trim(), '-5.00')
+  assert.equal(this.$('p.text--minus').text().trim(), toLocaleString([-5]))
 
   this.render(hbs`{{molecules/amount-change new=10 old=5}}`)
-  assert.equal(this.$('p').text().trim(), '5.00')
+  assert.equal(this.$('p').text().trim(), toLocaleString([5]))
 
   this.render(hbs`{{molecules/amount-change new=10 old=10}}`)
   assert.equal(this.$('p').text().trim(), '')
