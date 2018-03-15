0b0ddc4d837129ac8399a5b15e159aabaa31ff61
Marcin Jakubowski <marcin.jakubowski@netguru.pl>
2018-03-15 12:31:32 +0100

    Add time-input component

---
+++ addon/components/molecules/time-input.js
@@ -0,0 +1,38 @@
+import Component from '@ember/component'
+import { durationAsHms } from 'echo-ember-common/helpers/duration-as-hms'
+import layout from '../../templates/components/molecules/time-input'
+
+export default Component.extend({
+  layout,
+
+  tagName: 'fieldset',
+  classNames: 'form__fieldset time-input',
+  'data-test-time-input': true,
+
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+
+  didReceiveAttrs() {
+    if (!this.get('value')) return
+
+    const [
+      hours, minutes, seconds,
+    ] = durationAsHms([this.get('value')], { showHours: true })
+      .split(':')
+      .map(item => parseInt(item, 10))
+
+    this.setProperties({ hours, minutes, seconds })
+  },
+
+  actions: {
+    change(type, value) {
+      const parsedValue = parseInt(value, 10)
+      this.set(type, isNaN(parsedValue) ? null : parsedValue)
+
+      const duration = this.get('seconds') * 1 + this.get('minutes') * 60 + this.get('hours') * 3600
+
+      this.get('onChange')(duration)
+    }
+  },
+})
