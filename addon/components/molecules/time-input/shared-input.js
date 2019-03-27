745314cec2830f3416724ee1952b3619987c033c
Paweł Krauze <pawel.krauze@netguru.pl>
2019-03-27 13:51:01 +0100

    Add preventing input special characters in number fields

--- addon/components/molecules/time-input/shared-input.js
+++ addon/components/molecules/time-input/shared-input.js
@@ -4,4 +4,13 @@ import layout from '../../../templates/components/molecules/time-input/shared-in
 export default Component.extend({
   layout,
   tagName: '',
+
+  actions: {
+    onlyNumbers() {
+      if (event.which < 48 || event.which > 57)
+      {
+        event.preventDefault()
+      }
+    }
+  }
 })
