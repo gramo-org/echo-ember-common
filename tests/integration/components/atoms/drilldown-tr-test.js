7b97c1a4498b8fc8c6d25f51da6f50267bb569b7
SergiuV <vsergiu93@gmail.com>
2019-04-12 16:19:40 +0300

    test: enable optional features and refactor drilldown-tr tests

--- tests/integration/components/atoms/drilldown-tr-test.js
+++ tests/integration/components/atoms/drilldown-tr-test.js
@@ -2,10 +2,39 @@ import { module, test } from 'qunit'
 import { setupRenderingTest } from 'ember-qunit'
 import { render } from '@ember/test-helpers'
 import hbs from 'htmlbars-inline-precompile'
+import { findAll, find, click } from 'ember-native-dom-helpers'
+import {
+  create,
+  isVisible,
+  collection,
+  clickable
+} from 'ember-cli-page-object'
+
+const component = create({
+  tableRows: collection('table tr', {}),
+  firstLVL: {
+    visible: isVisible('table tr td:contains(lvl1)'),
+    click: clickable('table tr td:contains(lvl1)')
+  },
+  orphanLVL: {
+    visible: isVisible('table tr td:contains(orphan lvl2)')
+  },
+  secondLVL: {
+    click: clickable('table tr td:contains(lvl2)')
+  },
+  thirdLVL: {
+    visible: isVisible('table tr td:contains(lvl3)')
+  }
+})
 
 module('Integration | Component | atoms/drilldown tr', function(hooks) {
   setupRenderingTest(hooks)
-
+  hooks.beforeEach(function() {
+    component.setContext(this)
+  })
+  hooks.afterEach(function() {
+    component.removeContext()
+  })
   /* eslint-disable no-magic-numbers */
   test('it renders', async function(assert) {
     // Template block usage:
@@ -18,17 +47,27 @@ module('Integration | Component | atoms/drilldown tr', function(hooks) {
       {{#atoms/drilldown-tr level=2}}<td>lvl2</td>{{/atoms/drilldown-tr}}
       </table>
     `)
-
-    assert.equal(this.$('table tr:visible').length, 2, 'has only top level and orphan row initially shown')
-    assert.ok(this.$('table tr:visible td:contains(lvl1)').length, 'top level row initially shown')
-    assert.ok(this.$('table tr:visible td:contains(orphan lvl2)').length, 'orphan row initially shown')
-
-    this.$('td:contains(lvl1)').click()
-    assert.equal(this.$('table tr:visible').length, 4, 'shows next level when clicked')
-    assert.notOk(this.$('table tr:visible td:contains(lvl3)').length, 'shows next level when clicked')
-
-    this.$('td:contains(lvl2)').click()
-    assert.equal(this.$('table tr:visible').length, 5, 'shows last level when clicked')
-    assert.ok(this.$('table tr:visible td:contains(lvl3)').length, 'shows last level when clicked')
+    assert.async()
+    assert.equal(
+      component.tableRows.filter(row => row.isVisible).length,
+      2,
+      'has only top level and orphan row initially shown'
+    )
+    assert.ok(component.firstLVL.visible, 'top level row initially shown')
+    assert.ok(component.orphanLVL.visible, 'orphan row initially shown')
+    await component.firstLVL.click()
+    assert.equal(
+      component.tableRows.filter(row => row.isVisible).length,
+      4,
+      'shows next level when clicked'
+    )
+    assert.notOk(component.thirdLVL.visible, 'does not show the third lvl yet')
+    await component.secondLVL.click()
+    assert.equal(
+      component.tableRows.filter(row => row.isVisible).length,
+      5,
+      'shows last level when clicked'
+    )
+    assert.ok(component.thirdLVL.visible, 'shows last level when clicked')
   })
 })
