c14b37ebffde96b384d90c5b0a6da5f1e49c3466
SergiuV <vsergiu93@gmail.com>
2019-04-12 17:56:08 +0300

    test: fix mixing and failing tests

--- tests/unit/mixins/handles-http-status-error-test.js
+++ tests/unit/mixins/handles-http-status-error-test.js
@@ -1,62 +1,73 @@
 import EmberObject from '@ember/object'
 import HandlesHttpStatusErrorMixin from 'echo-ember-common/mixins/handles-http-status-error'
 import { module, test } from 'qunit'
+import Service from '@ember/service'
+import { setupTest } from 'ember-qunit'
+import { inject as service } from '@ember/service'
 
-module('Unit | Mixin | handles http status error', function() {
-  class I18n {
+module('Unit | Mixin | handles http status error', function(hooks) {
+  setupTest(hooks)
+  const I18n = Service.extend({
     t(key) {
       return `${key} - translated`
     }
-  }
+  })
 
-  class Session {
-    constructor() {
+  const Session = Service.extend({
+    init(...args) {
+      this._super(...args)
       this.invalidated = false
-    }
-
+    },
     invalidate() {
       this.invalidated = true
     }
-  }
+  })
 
-  class FlashMessages {
-    constructor() {
+  const FlashMessages = Service.extend({
+    init(...args) {
+      this._super(...args)
       this.clear()
-    }
-
+    },
     error(msg) {
       this.errorMessages.push(msg)
-    }
-
+    },
     clear() {
       this.errorMessages = []
     }
-  }
-
-  function createSubject() {
+  })
+  hooks.beforeEach(function() {
     const HandlesHttpStatusErrorObject = EmberObject.extend(HandlesHttpStatusErrorMixin, {
-      i18n: new I18n(),
-      flashMessages: new FlashMessages(),
-      session: new Session(),
-
       transitionTo(route) {
         this.transitionedTo = route
-      }
+      },
+      session: service()
     })
+    this.owner.register('test:subject', HandlesHttpStatusErrorObject)
+    this.owner.register('service:i18n', I18n)
+    this.owner.register('service:session', Session)
+    this.owner.register('service:flashMessages', FlashMessages)
+    this.subject = this.owner.lookup('test:subject')
+  })
 
-    return HandlesHttpStatusErrorObject.create()
-  }
+  // function createSubject() {
+  //   const HandlesHttpStatusErrorObject = EmberObject.extend(HandlesHttpStatusErrorMixin, {
+  //     transitionTo(route) {
+  //       this.transitionedTo = route
+  //     }
+  //   })
+  //   return HandlesHttpStatusErrorObject.create()
+  // }
 
   test('does nothing on no errors', function(assert) {
     const error = { errors: [] }
-    const subject = createSubject()
+    const subject = this.subject
 
     assert.equal(subject.handleHttpStatusError(error), true, 'does nothing when no errors')
   })
 
   test('does nothing on 500 internal server error', function(assert) {
     const error = { errors: [{ status: '500' }] }
-    const subject = createSubject()
+    const subject = this.subject
 
     assert.equal(subject.handleHttpStatusError(error), true, 'does nothing when no errors')
     assert.deepEqual(subject.get('flashMessages.errorMessages'), [], 'no error flash')
@@ -64,7 +75,7 @@ module('Unit | Mixin | handles http status error', function() {
 
   test('handles 401 unauthorized error', function(assert) {
     const error = { errors: [{ status: '401' }] }
-    const subject = createSubject()
+    const subject = this.subject
 
     assert.equal(subject.handleHttpStatusError(error), false, 'is handled by mixin')
     assert.deepEqual(
@@ -79,7 +90,7 @@ module('Unit | Mixin | handles http status error', function() {
 
   test('handles 409 conflict error', function(assert) {
     const error = { errors: [{ status: '409' }] }
-    const subject = createSubject()
+    const subject = this.subject
 
     assert.equal(subject.handleHttpStatusError(error), false, 'is handled by mixin')
     assert.deepEqual(
@@ -91,7 +102,7 @@ module('Unit | Mixin | handles http status error', function() {
 
   test('handles 503 conflict error', function(assert) {
     const error = { errors: [{ status: '503' }] }
-    const subject = createSubject()
+    const subject = this.subject
 
     assert.equal(subject.handleHttpStatusError(error), false, 'is handled by mixin')
     assert.deepEqual(
